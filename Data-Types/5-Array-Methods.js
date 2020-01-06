5a2979f7e3280999405c246897e905df7fd0521b
Mikhaylov Danil <f1rstaft3rme@gmail.com>
2020-01-06 18:36:02 +0300

    12345

--- Data-Types/5-Array-Methods.js
+++ Data-Types/5-Array-Methods.js
@@ -333,7 +333,8 @@ arr.includes(item, from) – ищет item, начиная с индекса fro
 // console.log(array); // [8, 5, 2, 1, -10]
 
 
-let copySortedArray;
-let copySorted = array => copySortedArray = array.sort();
-
-console.log(copySorted(["HTML", "JavaScript", "CSS"])); // ["CSS", "HTML", "JavaScript"]
+// let copySortedArray;
+//
+// let copySorted = array => copySortedArray = array.sort();
+//
+// console.log(copySorted(["HTML", "JavaScript", "CSS"])); // ["CSS", "HTML", "JavaScript"]
