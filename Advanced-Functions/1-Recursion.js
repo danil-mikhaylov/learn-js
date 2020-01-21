5d3b4051d1f0e876f3617db7fede9514e9355370
mitmih <38176228+mitmih@users.noreply.github.com>
2020-01-21 21:53:09 +0300

    recursion-finished

--- Advanced-Functions/1-Recursion.js
+++ Advanced-Functions/1-Recursion.js
@@ -73,17 +73,116 @@
 // list = {value: 0, next: list};
 // console.log(list); // { value: 1, next: { value: 2, next: { value: 3, next: [Object] } } }
 
-
 // Homework
 
-function sumToCycle(n) {
+function sumToCycle(number) {
   let sum = 0;
-  for (let i = 0; i < n; i++) {
+  for (let i = number; i > 0; i--) {
     sum += i;
   }
-  return (n === 1) ? n : sum;
+  return sum;
 }
-function sumToRecursion(n) {
 
+function sumToRecursive(number) {
+  return number === 1 ? number : number + sumToRecursive(number - 1);
 }
-console.log(sumToRecursion(5));
+
+function sumTo(number) {
+  return number * (number + 1) / 2
+}
+
+console.log(sumToCycle(5)); // 15
+console.log(sumToRecursive(100000)); // 15
+console.log(sumTo(5)); // 15
+
+function factorial(number) {
+  let res =  number;
+  for (let i = number - 1; i > 0; i--) {
+    res = res * i;
+  }
+  return res;
+}
+
+function factorial(number) {
+return number ? number * factorial(number - 1) : 1;
+return (number === 1) ? number : number * factorial(number - 1);
+}
+
+console.log(factorial(5)); // 120
+
+function fib(number) {
+  let a = 1;
+  let b = 2;
+  for (let i = 3; i <= number; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return a;
+}
+console.log(fib(3)); // 2
+console.log(fib(193372)); // 13
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printList(list) {
+  while (list) {
+    console.log(list.value);
+    list = list.next;
+  }
+}
+
+printList(list);
+
+function printList(list) {
+  console.log(list.value);
+  if (list.next) {
+    return printList(list.next);
+  }
+}
+printList(list);
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+let arr = [];
+function printReverseList(list) {
+  arr.push(list.value);
+  if (list.next) {
+    return printReverseList(list.next)
+  }
+  arr.reverse();
+  for (let number of arr) {
+    console.log(number);
+  }
+}
+function printReverseList(list) {
+  if (list.next) {
+    printReverseList(list.next);
+  }
+  console.log(list.value);
+}
+printReverseList(list);
