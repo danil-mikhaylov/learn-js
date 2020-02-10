c08738ae7163e0373f97467a8eb3d974053b477a
mitmih <38176228+mitmih@users.noreply.github.com>
2020-02-10 11:41:09 +0300

    Comprasion finished

--- First-Steps/2-Comparison.js
+++ First-Steps/2-Comparison.js
@@ -1,29 +1,23 @@
-// ИСПОЛЬЗОВАНИЕ НОВОГО js
-'use strict';
-
-// Операторы сравнения
+// Comparison operators
 
 console.log(1 > 2); // true
 console.log(2 == 1); // false
 console.log(2 != 1); // true
 
-console.log('2' > 1); // true
-console.log('01' == 1); // true
+console.log("2" > 1); // true
+console.log("01" == 1); // true
 
 let a = 0;
-console.log( Boolean(a) ); // false
+console.log(Boolean(a)); // false
 let b = "0";
-console.log( Boolean(b) ); // true
+console.log(Boolean(b)); // true
 console.log(a == b); // true!
 
 console.log(0 == false); // true
-console.log('' == false); //true
+console.log("" == false); //true
 
-// "===" - Сравнение без приведения типов
+// "===" - Comparison without type conversions
 
-console.log(0 === false); // false, так как сравниваются разные типы
+console.log(0 === false); // false, as different types are compared
 console.log(null == undefined); // true
 console.log(null === undefined); // false
-
-
-
