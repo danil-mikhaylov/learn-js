0645e21f41813c32bdc8d8c45c86118c5132e242
mitmih <38176228+mitmih@users.noreply.github.com>
2020-02-25 19:19:23 +0300

    6.2.0

--- 6.Prototypes/2.function-prototype.js
+++ 6.Prototypes/2.function-prototype.js
@@ -1,13 +1,32 @@
-let animal = {
-  eats: true
-};
+// let animal = {
+//   eats: true
+// };
+//
+// function Rabbit(name) {
+//   this.name = name;
+// }
+//
+// Rabbit.prototype = animal;
+//
+// let rabbit = new Rabbit("White Rabbit");
+//
+// console.log(rabbit.eats); // true
+/*
+  Установка Rabbit.prototype = animal буквально говорит интерпретатору следующее:
+  "При создании объекта через new Rabbit() запиши ему animal в [[Prototype]]".
+ */
 
-function Rabbit(name) {
-  this.name = name;
+// F.prototype по умолчанию, свойство constructor
+
+// function Rabbit() {
+//   /* Прототип по умолчанию
+//   // Rabbit.prototype = {constructor: Rabbit};
+//    */
+// }
+
+function Rabbit() {
+  // по умолчанию:
+  // Rabbit.prototype = { constructor: Rabbit }
 }
 
-Rabbit.prototype = animal;
-
-let rabbit = new Rabbit("White Rabbit"); // rabbit.__proto__ == animal
-
-console.log(rabbit.eats); // true
+console.log(Rabbit.prototype.constructor == Rabbit); // true
