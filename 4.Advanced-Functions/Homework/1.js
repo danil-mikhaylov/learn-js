de271a4d7753d1c1ff6afc1d01e195998e7cb570
Danil Mikhaylov <f1rstaft3rme@gmail.com>
2020-03-09 11:25:08 +0300

    1

--- 4.Advanced-Functions/Homework/1.js
+++ 4.Advanced-Functions/Homework/1.js
@@ -1,11 +1,10 @@
-console.time("form");
+// Вычисляем сумму чисел до данного
 function sumTo(number) {
+  // Здесь используем формулу
   return (number * (number + 1)) / 2;
 }
 console.log(sumTo(10000)); // 50005000
-console.timeEnd("form"); // form: 4.141ms
 
-console.time("cycle");
 function sumToCycle(number) {
   let sum = 0;
   for (let i = number; i > 0; i--) {
@@ -14,14 +13,13 @@ function sumToCycle(number) {
   return sum;
 }
 console.log(sumToCycle(10000)); // 50005000
-console.timeEnd("cycle"); // cycle: 0.225ms
 
-console.time("recursive");
 function sumToRecursive(number) {
+  // База рекурсии -- number === 1 ? number;
+  // Когда дойдёт до того, что number === 1, начнут возвращаться все значения
   return number === 1 ? number : number + sumToRecursive(number - 1);
 }
 console.log(sumToRecursive(10000)); // 50005000
-console.timeEnd("recursive"); // recursive: 0.913m
 
 function factorial(number) {
   let res = number;
@@ -39,14 +37,12 @@ function factorial(number) {
 console.log(factorial(5)); // 120
 
 function fib(number) {
-  let a = 1;
-  let b = 2;
-  let counter = 0;
-  for (let i = 3; i <= number; i++) {
-    let c = a + b;
+  let a = 1; // Первое число
+  let b = 2; // Второе число
+  for (let i = 3; i <= number; i++) { // Сделает (number - 2) раз
+    let c = a + b; // Их сумма
     a = b;
     b = c;
-    counter += 1;
   }
   return a;
 }
@@ -67,20 +63,21 @@ let list = {
   }
 };
 
-function printList(list) {
-  while (list) {
-    console.log(list.value);
-    list = list.next;
-  }
-}
+// function printList(list) {
+//   while (list) {
+//     console.log(list.value);
+//     list = list.next;
+//   }
+// }
+//
+// printList(list);
 
-printList(list);
-
 function printList(list) {
-  console.log(list.value);
-  if (list.next) {
-    return printList(list.next);
+  console.log(list.value); // Сначала выводим текущее значение
+  if (list.next) { // Если можем идти дальше по списку
+    return printList(list.next); // То вызываем функцию для следующего подсписка
   }
+  // В итоге она выводит значение следующего подсписка, идёт к следующему-следующему...
 }
 printList(list);
 
@@ -97,22 +94,22 @@ let list = {
     }
   }
 };
-
-let arr = [];
-function printReverseList(list) {
-  arr.push(list.value);
-  if (list.next) {
-    return printReverseList(list.next);
-  }
-  arr.reverse();
-  for (let number of arr) {
-    console.log(number);
-  }
-}
+//
+// let arr = [];
+// function printReverseList(list) {
+//   arr.push(list.value);
+//   if (list.next) {
+//     return printReverseList(list.next);
+//   }
+//   arr.reverse();
+//   for (let number of arr) {
+//     console.log(number);
+//   }
+// }
 function printReverseList(list) {
   if (list.next) {
     printReverseList(list.next);
   }
-  console.log(list.value);
+  console.log(list.value); // Здесь наоборот: выводим в конце
 }
 printReverseList(list);
