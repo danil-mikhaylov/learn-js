2c69286cf81b31bd843721df8ed672604f8b38e6
user <f1rstaft3rme@gmail.com>
2020-03-27 16:28:03 +0300

    9.5 finished. There's no homework.

--- 3.Data-Types/Homework/7.js
+++ 3.Data-Types/Homework/7.js
@@ -18,7 +18,7 @@ readMessages.add(messages[0]);
 // readMessages до сих пор содержит 2 элемента
 
 // Вопрос: было ли сообщение message[0] прочитано?
-alert("Read message 0: " + readMessages.has(messages[0])); // true
+console.log("Read message 0: " + readMessages.has(messages[0])); // true
 
 messages.shift();
 // теперь readMessages содержит 1 элемент (хотя технически память может быть очищена позже)
---
+++ 7.Classes/5.extend-natives.js
@@ -0,0 +1,44 @@
+// От Array, Map и т.д. тоже можно наследовать:
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty()); // false
+
+let filteredArr = arr.filter((item) => item >= 10);
+console.log(filteredArr); // false
+console.log(filteredArr.isEmpty()); // false
+
+// Как я понял, встроенные методы (filter, map...) возвращают именно объект PowerArray, а не Array
+// Можем это исправить (это не ошибка!!!) вот так:
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  // встроенные методы массива будут использовать этот метод как конструктор
+  static get [Symbol.species]() {
+    return Array;
+    // return PowerArray;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty()); // false
+
+// filter создаст новый массив, используя arr.constructor[Symbol.species] как конструктор
+let filteredArr = arr.filter((item) => item >= 10);
+
+// filteredArr не является PowerArray, это Array
+console.log(filteredArr.isEmpty()); // Error: filteredArr.isEmpty is not a function
+
+/* Отсутствие статического наследования встроенных классов
+  У встроенных объектов есть собственные статические методы, например Object.keys, Array.isArray и т. д.
+  Обычно, когда один класс наследует другому, то наследуются и статические методы.
+  Но встроенные классы – исключение. Они не наследуют статические методы друг друга.
+  Например, и Array, и Date наследуют от Object, так что в их экземплярах доступны методы из Object.prototype.
+  Но Array.[[Prototype]] не ссылается на Object, поэтому нет методов Array.keys() или Date.keys().
+ */
