1d2a9338cf0fdbc6f05904ba8be89f5601f6684c
user <f1rstaft3rme@gmail.com>
2020-03-30 17:15:51 +0300

    12.2 is finished.

--- 9.Async/Homework/2.js
+++ 9.Async/Homework/2.js
@@ -11,3 +11,31 @@
 // secondPromise.then(console.log); // выведет "Done!" через секунду
 // secondPromise.then(result => console.log(result)); // выведет "Done!" через секунду
 
+/** Можно ли "перевыполнить" промис?
+ * Что выведет код ниже?
+ */
+
+// let promise = new Promise((resolve, reject) => {
+//   resolve(1); // 1, первый вызов
+//
+//   // Все последующие выводы resolve/reject проигнорируются
+//   setTimeout(() => resolve(2), 1000);
+// });
+//
+// promise.then(console.log);
+
+/** Задержка на промисах
+ * Встроенная функция setTimeout использует колбэк-функции.
+ * Создайте альтернативу, использующую промисы.
+ * Функция delay(ms) должна возвращать промис, который перейдёт в состояние «выполнен» через ms,
+ * так чтобы мы могли добавить к нему .then:
+ */
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+  // У resolve нет аргументов, потому что мы не возвращаем из delay ничего, а просто гарантируем задержку
+}
+
+delay(3000).then(() => console.log("Выполнилось через 3 секунды"));
+
+
